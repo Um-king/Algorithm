c56c6e2e684c732bab14fbd7d0644b69fe12f765
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-01-19 20:26:10 +0900

    Update 두 수의 차.py

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
@@ -1,8 +1,2 @@
 def solution(num1, num2):
-    num1=int(num1)
-    num2=int(num2)
-    if num1 and num2 < -50000 or num1 and num2 > 50000 :
-        print ("숫자는 마이너스 5만과 플러스 5만 사이로 넣어주세요")
-        exit
-    answer = num1-num2
-    return answer
+    return num1-num2
