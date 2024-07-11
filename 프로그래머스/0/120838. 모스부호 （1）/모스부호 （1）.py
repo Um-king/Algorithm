9700c6d050b8b024bfc08adca9578b9e39b79fd3
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-07-11 15:18:58 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -1,3 +1,17 @@
+# morse = { 
+#     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+#     '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+#     '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+#     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+#     '-.--':'y','--..':'z'
+# }
+
+# def solution(letter):
+#     word = letter.split(' ')
+#     result = [morse[i] for i in word]
+#     result = ''.join(result)
+#     return result
+
 morse = { 
     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
     '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
@@ -7,7 +21,6 @@ morse = {
 }
 
 def solution(letter):
-    word = letter.split(' ')
-    result = [morse[i] for i in word]
-    result = ''.join(result)
-    return result
+    return ''.join(map(lambda w : morse[w] , letter.split()))
+
+solution(".... . .-.. .-.. ---")
