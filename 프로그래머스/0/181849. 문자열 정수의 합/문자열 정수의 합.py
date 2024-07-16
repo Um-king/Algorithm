e413126a98f1857594f898d9c4ddaa9b56512e68
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-07-17 00:12:45 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -1,2 +1,7 @@
+# def solution(num_str):
+#     return sum([int(i) for i in num_str])
+
+
+# 문자열은 이미 리스트이기에 위처럼 list로 변환 안해도 된다.
 def solution(num_str):
-    return sum([int(i) for i in num_str])
+    return sum(map(int, list(num_str)))
