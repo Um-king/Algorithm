493daf2aa6faf7b6a32c7376fa8f79d531cf411a
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-07-25 11:34:33 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,4 +1,9 @@
+# def solution(array, n):
+#     # 대상, key=함수: 대상에 대해서 해당 함수 적용, 함수내 두번째 인자: 정렬 순서 
+#     # abs(x-n)가 같을 때 x값이 작은 순으로 정렬
+#     return min(array, key = lambda x: (abs(x-n),  x-n)) 
+
 def solution(array, n):
     # 대상, key=함수: 대상에 대해서 해당 함수 적용, 함수내 두번째 인자: 정렬 순서 
     # abs(x-n)가 같을 때 x값이 작은 순으로 정렬
-    return min(array, key = lambda x: (abs(x-n),  x-n)) 
+    return sorted(array, key = lambda x: (abs(x-n),  x-n))[0]
