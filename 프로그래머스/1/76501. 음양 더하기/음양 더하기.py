1cce397beeb381dfb69886774fc32efa14fc7186
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-05 10:48:33 +0900

    [level 1] Title: 음양 더하기, Time: 0.23 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,2 +1,6 @@
 def solution(absolutes, signs):
     return sum(map(lambda x : x[0] if x[1] else -x[0], zip(absolutes, signs)))
+
+
+# def solution(absolutes, signs):
+#     return sum(map(lambda x : absolutes[x] if signs[x] else -absolutes[x], range(len(absolutes))))
