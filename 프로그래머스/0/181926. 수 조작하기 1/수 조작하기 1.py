0bd3651027ff5f218c5dfc36a26e68cc85b3bfd8
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-13 13:20:59 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.40 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,3 +1,4 @@
 def solution(n, control):
     num = {'w':1, 's':-1, 'd':10, 'a':-10}
     return n + sum([num[i] for i in control])
+# return n + 10*(control.count('d')-control.count('a')) + (control.count('w')-control.count('s'))
