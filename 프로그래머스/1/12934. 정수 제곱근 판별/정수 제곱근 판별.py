1d95f7aba48d374f16b1c38cfa8af751c4c4a7a7
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-13 14:19:14 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,4 +1,5 @@
 import math
 
 def solution(n):
-    return (math.isqrt(n) + 1) ** 2 if math.isqrt(n) ** 2 == n else -1
+    # return (math.isqrt(n) + 1) ** 2 if math.isqrt(n) ** 2 == n else -1
+     return ((n ** .5) + 1) ** 2 if n ** .5 % 1 == 0 else -1
