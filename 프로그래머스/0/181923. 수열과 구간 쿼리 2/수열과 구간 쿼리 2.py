7a0749c95556098b7bde347ebc2c693c32556239
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-13 14:06:51 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 11.88 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -2,5 +2,5 @@ def solution(arr, queries):
     result_lst = []
     for i,j,k in queries:
         lst = [n for n in arr[i : j + 1] if n > k]
-        result_lst.append(min(lst)) if lst else result_lst.append(-1)
+        result_lst.append(min(lst) if lst else -1) 
     return result_lst
