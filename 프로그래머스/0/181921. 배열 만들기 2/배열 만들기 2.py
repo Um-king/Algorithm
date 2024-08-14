8c20424cd027ef3b2b3f5097df60de41f60d93c3
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-14 10:11:23 +0900

    [level 0] Title: 배열 만들기 2, Time: 276.61 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,2 +1,9 @@
 def solution(l, r):
     return [i for i in range(l, r + 1) if not str(i).replace('0', '').replace('5', '')] or [-1]
+
+    # 보편적 답 set을 사용(0과 5가 중복되지 않은 상태에서 뺏을 때 값이 비어있다면 해당 값은 0과 5로만 이루어진것)
+    # answer = []
+    # for i in range(l, r + 1):
+    #     if not set(str(i)) -  set(['0', '5']):
+    #         answer.append(i)
+    # return answer if answer else [-1]
