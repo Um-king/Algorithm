eab0fc7f513d242f5384db421cf9c59d8475c74c
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-20 18:44:07 +0900

    [level 0] Title: 세로 읽기, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,5 +1,5 @@
 def solution(my_string, m, c):
     answer = ''
-    for i in range(len(my_string) // m):
-        answer += my_string[i*m:i*m+m][c - 1]
+    for i in range(c-1,len(my_string), m):
+        answer += my_string[i]
     return answer
