2480536c7f9f109c949ea624b63c75bbe96003df
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-21 14:41:12 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.31 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,5 +1,5 @@
 def solution(my_string):
-    answer = [0 for i in range(52)]
+    answer = [0] * 52
     for i in my_string:
-        answer[ord(i) - 65 if ord(i) < 97 else ord(i) - 71] += 1
+        answer[ord(i) - 65 if i.isupper() else ord(i) - 71] += 1
     return answer
