f54f80bd522a9da79de491568c4317c2280eacf5
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-21 14:51:04 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,2 +1,2 @@
 def solution(my_string, indices):
-    return ''.join([my_string[i] for i in range(len(my_string)) if i not in indices])
+    return ''.join([j for i, j in enumerate(my_string) if i not in indices])
