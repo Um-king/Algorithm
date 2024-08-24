6a0f41d5818317aaabddba15c4064acd75da2a6e
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-08-24 18:29:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,4 +1,2 @@
 def solution(s):
-    if len(s) != 4 and len(s) != 6:
-        return False
-    return sum(list(map(lambda x: x.isdigit(), s))) == len(s)
+    return s.isdigit() and len(s) in [4, 6]
