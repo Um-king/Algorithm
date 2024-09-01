82bc7426b4b74666e3d49b5aad36f8cedb861677
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-01 22:11:34 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = []
+    for i in s.split():
+        if i != 'Z':
+            answer.append(int(i))
+        else:
+            answer.pop()
+    
+    return sum(answer)
