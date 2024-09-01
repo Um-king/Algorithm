76b6e3ddfa0a40cf9b538553a051164de2168c79
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-01 21:07:33 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,13 +1,3 @@
 def solution(my_string):
-    answer = 0
-    num = ""
-    for i in my_string:
-        if i.isdigit():
-            num += i
-        else:
-            if num:
-                answer += int(num)
-                num = ""
-    if num:
-        answer += int(num)
-    return answer
+    answer = ''.join([i if i.isdigit() else ' ' for i in my_string])
+    return sum([int(i) for i in answer.split()])
