2faa54fa65b0be3f5904a0f8f3ab6cf21b226bcd
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-01 23:09:47 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,22 +1,4 @@
 def solution(numbers):
-    dic = {
-        'one':'1',
-        'two':'2',
-        'three':'3',
-        'four':'4',
-        'five':'5',
-        'six':'6',
-        'seven':'7',
-        'eight':'8',
-        'nine':'9',
-        'zero':'0'
-    }
-
-    s = ""
-    answer = ""
-    for i in numbers:
-        s+=i
-        if s in dic:
-            answer += dic[s]
-            s = ""
-    return int(answer)
+    for i,j in enumerate(['zero','one','two','three','four','five','six','seven','eight','nine']):
+        numbers = numbers.replace(j, str(i))
+    return int(numbers)
