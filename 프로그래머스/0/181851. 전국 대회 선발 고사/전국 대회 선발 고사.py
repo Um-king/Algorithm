fb0384e40856d7d1c3525505666813843b57c75e
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-01 18:00:56 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,12 +1,3 @@
 def solution(rank, attendance):
-    cnt = 0
-    tmp = []
-    for i in range(1, len(rank) + 1):
-        index = rank.index(i)
-        if attendance[index]:
-            tmp.append(index)
-            cnt += 1
-
-        if cnt == 3:
-            break
-    return 10000 * tmp[0] + 100 * tmp[1] + tmp[2]
+    answer = sorted([(j, i) for i, j in enumerate(rank) if attendance[i]])
+    return 10000 * answer[0][1] + 100 * answer[1][1] + answer[2][1]
