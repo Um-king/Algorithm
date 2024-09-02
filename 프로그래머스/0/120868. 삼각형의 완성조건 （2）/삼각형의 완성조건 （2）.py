958332ebf45c1f9870fac90c21392cf90a74e602
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-02 16:57:49 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,9 +1,2 @@
 def solution(sides):
-    n, m = sorted(sides)
-    answer = 0
-    for i in range(m - n, m + 1):
-        if i + n > m:
-            answer += 1
-    for i in range(m + 1, n + m):
-        answer += 1
-    return answer
+    return sum(sides) - max(sides) + min(sides) - 1
