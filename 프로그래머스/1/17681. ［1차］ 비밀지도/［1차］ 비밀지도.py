87e1777e5d1d9645497beaff4e211ce7567d8eb4
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-04 01:40:21 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,9 +1,18 @@
+# def solution(n, arr1, arr2):
+#     answer = []
+
+#     for i in range(n):
+#         # 정수끼리 비트 연산을 통해 이진수로 변환하여 계산 후 다시 정수로 반환하는 개념
+#         result = bin(arr1[i] | arr2[i])[2:]
+#         answer.append(("0" * (n - len(result))) + result)
+
+#     return [i.replace("1", "#").replace("0", " ") for i in answer]
+
 def solution(n, arr1, arr2):
     answer = []
-
-    for i in range(n):
-        # 정수끼리 비트 연산을 통해 이진수로 변환하여 계산 후 다시 정수로 반환하는 개념
-        result = bin(arr1[i] | arr2[i])[2:]
-        answer.append(("0" * (n - len(result))) + result)
-
-    return [i.replace("1", "#").replace("0", " ") for i in answer]
+    for i, j in zip(arr1, arr2):
+        a = bin(i|j)[2:]
+        a = a.rjust(n, '0')
+        a = a.replace("1", "#").replace("0", " ")
+        answer.append(a)
+    return answer
