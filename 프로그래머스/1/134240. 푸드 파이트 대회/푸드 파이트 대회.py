ae6fb6c75974f5a5eca90a2716550f06f5d76744
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-04 01:10:14 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,6 +1,6 @@
 def solution(food):
-    answer = []
-    for i, j in enumerate(food):
-        for k in range(j // 2):
-            answer.append(str(i))
-    return ''.join(answer + ['0'] + answer[::-1])
+    answer = ""
+    for i, j in enumerate(food[1:]):
+        # 해당 인덱스가 어차피 1,2,3 계산된 것이니깐, 인덱스에 해당 음식 개수의 반개를 곱해서 문자열의 개수를 만든다..!
+        answer += (str(i+1)) * (j // 2)
+    return answer + "0" + answer[::-1]
