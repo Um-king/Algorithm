087df4bb2ebf1b24c4fcac9974164add9723069b
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-04 21:18:46 +0900

    [level 1] Title: 모의고사, Time: 5.85 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,41 @@
+# def solution(answers):
+#     l = len(answers)
+#     a = [1,2,3,4,5] * (l // 5) + [1,2,3,4,5][: (l% 5)]
+#     b = [2,1,2,3,2,4,2,5] * (l // 8) + [2,1,2,3,2,4,2,5][:(l%8)]
+#     c = [3,3,1,1,2,2,4,4,5,5] * (l // 10) + [3,3,1,1,2,2,4,4,5,5][:(l%10)]
+#     answer = []
+#     result = [0,0,0]
+#     for i, j in enumerate(answers):
+#         if j == a[i]:
+#             result[0] += 1
+#         if j == b[i]:
+#             result[1] += 1
+#         if j == c[i]:
+#             result[2] += 1
+    
+#     for i, j in enumerate(result):
+#         if max(result) == j:
+#             answer.append(i + 1)
+#     return answer
+
 def solution(answers):
-    l = len(answers)
-    a = [1,2,3,4,5] * (l // 5) + [1,2,3,4,5][: (l% 5)]
-    b = [2,1,2,3,2,4,2,5] * (l // 8) + [2,1,2,3,2,4,2,5][:(l%8)]
-    c = [3,3,1,1,2,2,4,4,5,5] * (l // 10) + [3,3,1,1,2,2,4,4,5,5][:(l%10)]
-    answer = []
+    a = [1,2,3,4,5] 
+    b = [2,1,2,3,2,4,2,5] 
+    c = [3,3,1,1,2,2,4,4,5,5] 
+
     result = [0,0,0]
+    answer = []
+
     for i, j in enumerate(answers):
-        if j == a[i]:
+        if j == a[i % len(a)]:
             result[0] += 1
-        if j == b[i]:
+        if j == b[i % len(b)]:
             result[1] += 1
-        if j == c[i]:
+        if j == c[i % len(c)]:
             result[2] += 1
     
     for i, j in enumerate(result):
         if max(result) == j:
             answer.append(i + 1)
+    
     return answer
