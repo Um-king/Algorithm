c12184bb28092b9acf27c9acfeedb5251e228b79
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-04 17:40:02 +0900

    [level 1] Title: 폰켓몬, Time: 1.11 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,3 +1,4 @@
 def solution(nums):
-    answer = set(nums)
-    return len(answer) if len(answer) <= len(nums) // 2 else len(nums) // 2
+    # answer = set(nums)
+    # return len(answer) if len(answer) <= len(nums) // 2 else len(nums) // 2
+    return min(len(nums) // 2, len(set(nums)))
