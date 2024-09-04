2d83f1cd02d280acfd1a74f5193dd1fefb5a2eb3
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-04 22:02:24 +0900

    [level 1] Title: 소수 찾기, Time: 1452.18 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -2,8 +2,8 @@ def solution(n):
     # n개의 숫자를 생성한다
     nums = set(i for i in range(2, n + 1))
 
-    for i in range(2, int(n**0.5) + 1): 
+    for i in range(2, n + 1): 
+        # 차집합을 통해 소수의 배수들을 전부 제거한다.
         nums -= set(j for j in range(i * 2, n + 1, i))
 
     return len(nums)
-    
