81362aa3eb3d3eb5426e1ea2c4850380e1a7f668
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-05 17:44:39 +0900

    [level 1] Title: 숫자 짝꿍, Time: 96.08 ms, Memory: 27.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,25 +1,38 @@
+# def solution(X, Y):
+#     answer = []
+#     d1 = {}
+#     d2 = {}
+#     s = sorted((set(X) & set(Y)), reverse=True)
+
+#     if len(s) == 0:
+#         return "-1"
+
+#     for i in X:
+#         if i in d1:
+#             d1[i] += 1
+#         else:
+#             d1[i] = 1
+    
+#     for i in Y:
+#         if i in d2:
+#             d2[i] += 1
+#         else:
+#             d2[i] = 1
+
+#     for i in s:
+#         answer.extend([int(i)] * min(d1[i], d2[i]))
+    
+#     return ''.join(map(str, answer)) if any(answer) else "0"
+
 def solution(X, Y):
-    answer = []
-    d1 = {}
-    d2 = {}
-    s = sorted((set(X) & set(Y)), reverse=True)
+    answer = ""
 
-    if len(s) == 0:
+    for i in range(9, -1, -1):
+        answer += str(i) * min(X.count(str(i)), Y.count(str(i)))
+    
+    if answer == "":
         return "-1"
-
-    for i in X:
-        if i in d1:
-            d1[i] += 1
-        else:
-            d1[i] = 1
-    
-    for i in Y:
-        if i in d2:
-            d2[i] += 1
-        else:
-            d2[i] = 1
-
-    for i in s:
-        answer.extend([int(i)] * min(d1[i], d2[i]))
-    
-    return ''.join(map(str, answer)) if any(answer) else "0"
+    elif len(answer) == answer.count("0"):
+        return "0"
+    else:
+        return answer
