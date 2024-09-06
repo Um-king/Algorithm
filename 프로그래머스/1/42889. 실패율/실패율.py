8df367d49baa2cddd7e70484ef77e724fcc7e0af
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-07 00:10:16 +0900

    [level 1] Title: 실패율, Time: 1670.50 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,11 +1,10 @@
 def solution(N, stages):
     answer = []
-    people = len(stages)
+    p = len(stages)
 
     for i in range(1, N+1):
         cnt = stages.count(i)
-        loss = 0 if cnt == 0 else cnt/people
+        loss = 0 if cnt == 0 else cnt / p
         answer.append((i, loss))
-        people -= cnt
-
-    return [i for i, j  in sorted(answer, key=lambda x:x[1], reverse=True)]
+        p -= cnt
+    return [i for i, _ in sorted(answer, key=lambda x:x[1], reverse=True)]
