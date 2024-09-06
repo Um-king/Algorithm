93efe377c602d5d31cc8288b2f85f5dec7ad86d1
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-06 23:04:16 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,9 +1,9 @@
-def gcd(x ,y):
+def gcd(x, y):
     while y != 0:
         x, y = y, x%y
-    return x
+    return x 
 
 def solution(n, m):
-    gcd_value = gcd(n, m)
-    lcm_value = (n * m) // gcd_value
+    gcd_value = gcd(n, m) # 최대공약수
+    lcm_value = (n * m) // gcd_value # 최소공배수
     return [gcd_value, lcm_value]
