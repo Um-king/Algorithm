d0cf5d289581fe40e9690da8147092f3ddc9e973
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-06 23:19:53 +0900

    [level 1] Title: 시저 암호, Time: 2.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -3,12 +3,8 @@ def solution(s, n):
     for i in s:
         if i == " ":
             answer += i
-        
         elif i.isupper():
-            print(ord(i), ord('A'))
             answer += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
         elif i.islower():
-            print(ord(i), ord('a'))
             answer += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
-
     return answer
