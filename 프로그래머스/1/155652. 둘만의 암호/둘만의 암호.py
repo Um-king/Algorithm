c759958f317fbaa2b92649e63b48f11b01568d34
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-07 00:26:46 +0900

    [level 1] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,3 +1,3 @@
 def solution(s, skip, index):
     alpha = [chr(i) for i in range(ord('a'), ord('z') + 1) if chr(i) not in skip]
-    return ''.join([alpha[(alpha.index(i) + index) % len(alpha)] for i in s if i in alpha])
+    return ''.join([alpha[(alpha.index(i) + index) % len(alpha)] for i in s])
