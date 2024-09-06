aec4d507f704e6264db88120241472366c66dead
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-06 23:33:13 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.34 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,26 +1,13 @@
-# def solution(k, score):
-#     result = [] # 명예의 전당
-#     answer = [] # 최하위 점수
-#     for i in score:
-#         result.append(i)
-#         result.sort(reverse=True)
-
-#         if len(result)> k:
-#             result.pop() # 최하위 점수를 제거
-        
-#         answer.append(result[-1])
-    
-#     return answer
-
 def solution(k, score):
-    answer = []
-    result = []
+    result = [] # 명예의 전당
+    answer = [] # 발표 점수
 
     for i in score:
         result.append(i)
-
         if len(result) > k:
             result.remove(min(result))
-        
         answer.append(min(result))
+
     return answer
+
+print(solution(3, [10, 100, 20, 150, 1, 100, 200]))
