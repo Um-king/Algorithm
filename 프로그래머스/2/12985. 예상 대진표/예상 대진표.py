6a0413c3db5da6abf0c78dfdb59423760ed96014
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-09-06 16:15:40 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,14 +1,20 @@
-def solution(n,a,b):
-    answer = 1
-    x, y = min(a, b), max(a, b)
-    n = bin(n)[2:]
-    num = len(n) - (n.index("1") + 1)
-    while answer < num:
-        if x%2 and x + 1 == y:
-            return answer
+# def solution(n,a,b):
+#     answer = 1
+#     x, y = min(a, b), max(a, b)
+#     n = bin(n)[2:]
+#     num = len(n) - (n.index("1") + 1)
+#     while answer < num:
+#         if x%2 and x + 1 == y:
+#             return answer
 
-        x = x // 2 + x % 2
-        y = y // 2 + y % 2
+#         x = x // 2 + x % 2
+#         y = y // 2 + y % 2
         
+#         answer += 1
+#     return answer
+def solution(n, a, b):
+    answer = 0
+    while a!=b:
         answer += 1
+        a, b = (a+1)//2, (b+1)//2
     return answer
