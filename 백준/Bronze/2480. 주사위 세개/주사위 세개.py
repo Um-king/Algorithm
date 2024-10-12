301bc8bfbb466442fec5b2fffd8f05d77bca3760
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-10-12 18:26:29 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,18 +1,10 @@
-dice = list(map(int, input().split()))
+lst = list(map(int, input().split()))
+num_cnt = 0
+num = 0
+for i in lst:
+    num_value = lst.count(i)
+    if num_cnt < num_value:
+        num_cnt = num_value
+        num = i
 
-max_count = 0
-max_value = 0
-
-for i in range(1, 7):
-    count = dice.count(i)
-
-    if max_count < count:
-        max_count = count
-        max_value = i
-    
-if max_count == 3:
-    print(10000 + max_value * 1000)
-elif max_count == 2:
-    print(1000 + max_value * 100)
-elif max_count == 1:
-    print(100 * max(dice))
+print(10000 + num * 1000) if num_cnt == 3 else print(1000 + num * 100) if num_cnt == 2 else print(max(lst) * 100)
