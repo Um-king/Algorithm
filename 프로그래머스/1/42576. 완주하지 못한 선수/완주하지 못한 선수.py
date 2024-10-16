e1f73daa7eaa53469ac01972b757f904949c8b52
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-10-16 21:24:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.01 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,25 +1,8 @@
-# def solution(participant, completion):
-#     d = {}
-
-#     for i in participant:
-#         if i in d:
-#             d[i] += 1
-#         else:
-#             d[i] = 1
-    
-#     for i in completion:
-#         if i in d:
-#             d[i] -= 1
-
 def solution(participant, completion):
     participant.sort()
     completion.sort()
-
-    for i,j in zip(participant, completion):
-        if i != j:
-            return i
-    return participant[-1] 
-        
-
-    return list(filter(lambda x:d[x] == 1, d.keys()))[0]
     
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
