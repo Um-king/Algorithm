f1e47df07e923cc147ce57d723458ff97da4ff4d
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-10-18 14:57:08 +0900

    [level 1] Title: 과일 장수, Time: 108.06 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,10 +1,7 @@
-# def solution(k, m, score):
-#     answer = 0
-#     score.sort(reverse=True)
-#     for i in range(len(score) // m): # m만큼 배열을 나눠서 합을 구해야하니깐
-#         box = score[m*i:m*(i+1)]
-#         answer += box[-1] * m
-#     return answer
-
 def solution(k, m, score):
-    return sum(sorted(score)[len(score) % m::m]) * m
+    answer = 0
+    score.sort(reverse=True)
+
+    for i in range(len(score) // m):
+        answer += score[m*i:m*(i+1)][-1] * m
+    return answer
