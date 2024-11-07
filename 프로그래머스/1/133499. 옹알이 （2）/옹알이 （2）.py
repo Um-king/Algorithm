87ff167c944ac842ca8414c22091c25a53363c83
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-11-07 21:39:38 +0900

    [level 1] Title: 옹알이 (2), Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,30 +1,11 @@
-# def solution(babbling):
-#     answer = 0
-#     # 연속되는 말은 못하니 최소 연속되는 문자로 만들어서 문자열을 바꿔주면 됨
-#     can_s = ["aya", "ye", "woo", "ma"]
-#     cant_s = ["ayaaya", "yeye", "woowoo", "mama"]
-
-#     for i in babbling:
-#         for j in cant_s:
-#             if j in i:
-#                 i = 'x'
-        
-#         for k in can_s:
-#             i = i.replace(k, ' ')
-#         i = i.replace(' ', '')
-        
-#         if len(i) == 0:
-#             answer += 1
-#     return answer
-
 def solution(babbling):
     answer = 0
 
     for i in babbling:
-        for j in ['aya', 'ye', 'woo', 'ma']:
-            if j*2 not in i: # 만약 연속되는 문자가 없다면 -> 3개든 4개든 n개든 어차피 2개 이상부터 안됨
+        for j in ["aya", "ye", "woo", "ma"]:
+            if j*2 not in i:
                 i = i.replace(j, ' ')
-        if len(i.strip()) == 0:
+        if len(i.split()) == 0:
             answer += 1
 
-    return answer 
+    return answer
