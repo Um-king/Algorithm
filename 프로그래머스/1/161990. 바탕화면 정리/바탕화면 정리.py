d00f2a03c7153f69bfb9336d657ee0292bfef56e
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-11-07 22:23:45 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.30 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,13 +1,24 @@
+# def solution(wallpaper):
+#     answer = []
+#     lux, luy, rdx, rdy = 0, 0, 0, 0
+
+#     for i, word in enumerate(wallpaper):
+#         for j, file in enumerate(word):
+#             if file == "#":
+#                 answer.append((i, j))
+
+#     lux, luy = min(answer, key=lambda x:x[0])[0], min(answer, key=lambda x:x[1])[1]
+#     rdx, rdy = max(answer, key=lambda x:x[0])[0] + 1, max(answer, key=lambda x:x[1])[1] + 1
+
+#     return [lux, luy, rdx, rdy]
+
 def solution(wallpaper):
-    answer = []
-    lux, luy, rdx, rdy = 0, 0, 0, 0
+    a, b = [], []
 
     for i, word in enumerate(wallpaper):
         for j, file in enumerate(word):
             if file == "#":
-                answer.append((i, j))
+                a.append(i)
+                b.append(j)
 
-    lux, luy = min(answer, key=lambda x:x[0])[0], min(answer, key=lambda x:x[1])[1]
-    rdx, rdy = max(answer, key=lambda x:x[0])[0] + 1, max(answer, key=lambda x:x[1])[1] + 1
-
-    return [lux, luy, rdx, rdy]
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
